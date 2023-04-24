f92378fb2de6390875b0f2645f798c6a903c5a92
Geepeey <111070276+Geepeey@users.noreply.github.com>
2023-04-24 21:59:21 +0100

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,33 +1,25 @@
 #!/usr/bin/python3
+
 """Exports to-do list information for a given employee ID to CSV format.
 
 Usage: python3 export_todo_csv.py USER_ID
 
 USER_ID: The ID of the user whose to-do list will be exported.
 """
+
 import csv
 import requests
 import sys
+
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
     user_url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(user_url + "users/{}".format(user_id)).json()
     user_name = user.get("username")
-    user_todos = requests.get(
-        user_url + "todos", params={"userId": user_id}).json()
+    user_todos = requests.get(user_url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump(
-            {
-                user_id: [
-                    {
-                        "task": t.get("title"),
-                        "completed": t.get("completed"),
-                        "username": user_name,
-                    }
-                    for t in user_todos
-                ]
-            },
-            jsonfile,
-        )
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for todo in user_todos:
+            writer.writerow([user_id, user_name, todo.get("completed"), todo.get("title")])
