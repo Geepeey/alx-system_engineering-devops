b3683e80cf817123f7390073c75df640ee0113e7
Godspower Udokpoh <111070276+Geepeey@users.noreply.github.com>
2023-05-09 18:07:42 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,7 @@ def number_of_subscribers(subreddit):
 
     if response.status_code == 200:
         data = response.json()
-        no_subs = data.get("data").get("subscribers")
+        no_subs = data["data"]["subscribers"]
         return no_subs
     else:
         return 0
